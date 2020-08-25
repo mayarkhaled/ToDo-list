a751d6a2940fea3fbe36e2b3b159fcbdfac8f48b
mayar khaled <miarkhaled3@gmail.com>
2020-08-25 03:27:23 +0200

    after pretting

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "ignorePatterns": ["node_modules"],
+    "rules": {
+    }
+};
--- src/DOM.js
+++ src/DOM.js
@@ -1,116 +1,110 @@
-import {list} from './index.js'
+import { list } from "./index.js";
 
-const maindiv = document.querySelector('#lists');
-const addnewList = document.querySelector('#addNewList');
+const maindiv = document.querySelector("#lists");
+const addnewList = document.querySelector("#addNewList");
 
-function startDOM(){
-    addnewList.addEventListener('click' , ()=>{
-        //get the list name and description 
-        let listName = window.prompt('Enter your todo list name..');
-        let listDescription = window.prompt('Enter the description..');
-        //create list dom card 
-        let newList =  list(listName , listDescription);
-        createListCard(newList);
-        
-    });
+function startDOM() {
+  addnewList.addEventListener("click", () => {
+    //get the list name and description
+    let listName = window.prompt("Enter your todo list name..");
+    let listDescription = window.prompt("Enter the description..");
+    //create list dom card
+    let newList = list(listName, listDescription);
+    createListCard(newList);
+  });
 }
 
-function createListCard(newList  ){
-    //labels
-    const titlelable = document.createElement('label');
-    const deslable = document.createElement('label');
-    let br = document.createElement('br');
-    titlelable.textContent = "List title : ";
-    deslable.textContent = "describtion : ";
+function createListCard(newList) {
+  //labels
+  const titlelable = document.createElement("label");
+  const deslable = document.createElement("label");
+  let br = document.createElement("br");
+  titlelable.textContent = "List title : ";
+  deslable.textContent = "describtion : ";
 
-    //
-    const div = document.createElement('div');
-    const nameLabel = document.createElement('label');
-    const desLabel = document.createElement('label');
-    const addtodoButton = document.createElement('button');
-    const datePicker = document.createElement('input');
-    const list = document.createElement('ul');
-    const deleteButton = document.createElement('button');
+  //
+  const div = document.createElement("div");
+  const nameLabel = document.createElement("label");
+  const desLabel = document.createElement("label");
+  const addtodoButton = document.createElement("button");
+  const datePicker = document.createElement("input");
+  const list = document.createElement("ul");
+  const deleteButton = document.createElement("button");
 
-    nameLabel.textContent = newList.name;
-    desLabel.textContent = newList.des;
-    datePicker.type = 'date';
-    addtodoButton.textContent = 'Add new task ';
-    deleteButton.textContent = 'delete';
+  nameLabel.textContent = newList.name;
+  desLabel.textContent = newList.des;
+  datePicker.type = "date";
+  addtodoButton.textContent = "Add new task ";
+  deleteButton.textContent = "delete";
 
-     NonEmptychecklist(newList , list)
+  NonEmptychecklist(newList, list);
 
-    addtodoButton.addEventListener('click', ()=>{
-        list.appendChild(todoButtonEvent());
-    });
+  addtodoButton.addEventListener("click", () => {
+    list.appendChild(todoButtonEvent());
+  });
 
-    datePicker.addEventListener('click' , () =>{
-        newList.date = datePicker.textContent;
-    });
+  datePicker.addEventListener("click", () => {
+    newList.date = datePicker.textContent;
+  });
 
+  deleteButton.addEventListener("click", () => {
+    div.remove();
+  });
 
-    deleteButton.addEventListener('click' , () =>{
-        div.remove();
-    });
+  div.style.backgroundColor = "#978D8A";
+  div.style.width = "60em";
+  div.appendChild(deleteButton);
+  div.appendChild(titlelable);
+  div.appendChild(nameLabel);
+  div.appendChild(br);
+  div.appendChild(deslable);
+  div.appendChild(desLabel);
 
-    div.style.backgroundColor = '#978D8A';
-    div.style.width = '60em';
-    div.appendChild(deleteButton);
-    div.appendChild(titlelable);
-    div.appendChild(nameLabel);
-    div.appendChild(br);
-    div.appendChild(deslable);
-    div.appendChild(desLabel);
-    
-    br =  document.createElement('br');
-    div.appendChild(br);
-    div.appendChild(datePicker);
-    br =  document.createElement('br');
-    div.appendChild(br);
-    div.appendChild(addtodoButton);
-    div.appendChild(list);
-    maindiv.appendChild(div);
-
+  br = document.createElement("br");
+  div.appendChild(br);
+  div.appendChild(datePicker);
+  br = document.createElement("br");
+  div.appendChild(br);
+  div.appendChild(addtodoButton);
+  div.appendChild(list);
+  maindiv.appendChild(div);
 }
 
-function todoButtonEvent(){
-        let title = window.prompt('Enter the title'); 
-        let listEntry = [];
-        listEntry.push(title);
-        
-        const li = document.createElement('li');
-        const checkName = document.createElement('label');
-        const check = document.createElement('input');
-        check.type = 'checkbox';
-        checkName.textContent = title;
-        
-        check.addEventListener('click' , ()=>{
-            listEntry.push(check.checked);
-        });
+function todoButtonEvent() {
+  let title = window.prompt("Enter the title");
+  let listEntry = [];
+  listEntry.push(title);
 
-        li.appendChild(check);
-        li.appendChild(checkName);
-        return li;
-    
+  const li = document.createElement("li");
+  const checkName = document.createElement("label");
+  const check = document.createElement("input");
+  check.type = "checkbox";
+  checkName.textContent = title;
+
+  check.addEventListener("click", () => {
+    listEntry.push(check.checked);
+  });
+
+  li.appendChild(check);
+  li.appendChild(checkName);
+  return li;
 }
 
-function NonEmptychecklist(newList , list){
-    
+function NonEmptychecklist(newList, list) {
   //  const list = document.createElement('ul');
-    if(newList.checklist.length != 0){
-        for(let i = 0 ; i < newList.checklist.length ; i++){
-            
-            const li = document.createElement('li');
-            const checkName = document.createElement('label');
-            const check = document.createElement('input');
-            check.type = 'checkbox';
-            checkName.textContent = newList.checklist[i][0];
-            check.checked = newList.checklist[i][1];
-            li.appendChild(check);
-            li.appendChild(checkName);
-            list.appendChild(li);
-        }
+  if (newList.checklist.length != 0) {
+    for (let i = 0; i < newList.checklist.length; i++) {
+      const li = document.createElement("li");
+      const checkName = document.createElement("label");
+      const check = document.createElement("input");
+      check.type = "checkbox";
+      checkName.textContent = newList.checklist[i][0];
+      check.checked = newList.checklist[i][1];
+      li.appendChild(check);
+      li.appendChild(checkName);
+      list.appendChild(li);
     }
-    return list;
+  }
+  return list;
 }
-export {startDOM , createListCard};
+export { startDOM, createListCard };
